0336aae7e4b88e99771399ed1c0251b8f5060905
jkyyyy <jiangkunyun007@qq.com>
2021-05-01 12:32:48 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': 'src',
+        'common': '@/common',
+        'components': '@/components',
+        'assets': '@/assets',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  }
+}
